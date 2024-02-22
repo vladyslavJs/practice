9981dc6d7ca3e354fcf5c1bd4d9694d50fd602a5
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-02-22 23:26:10 +0100

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,64 @@
-export const App = () => {
-  const handleClick = () => {
-    console.log(Date.now());
+// export const App = () => {
+//   const handleClick = () => {
+//     console.log(Date.now());
+//   };
+//   return <div onClick={handleClick}>App</div>
+// }
+
+// Hooks React ------------------------------
+// import { useEffect, useState } from "react";
+// export const App = () => {
+//   const [first, setFirst] = useState(0);
+//   const [second, setSecond] = useState(0);
+
+//   useEffect(() => {
+//     console.log("First updated: ", first);
+//   }, [first]);
+
+//   useEffect(() => {
+//     console.log("Second updated: ", second);
+//   }, [second]);
+
+//   useEffect(() => {
+//     console.log("First or second updated: ", first + second);
+//   }, [first, second]);
+
+//   const reset = () => {
+//     setFirst(0) + setSecond(0);
+//   };
+
+//   return (
+//     <>
+//       <button onClick={() => setFirst(first + 1)}>First: {first}</button>
+//       <button onClick={() => setSecond(second + 1)}>Second: {second}</button>
+//       <button onClick={reset}>Reset All effects</button>
+//     </>
+//   );
+// };
+// Hooks React ------------------------------
+
+// import css from './App.module.css'
+import { FaReact } from "react-icons/fa";
+
+export default function App() {
+
+  //не потрібна валідація, використовується не контрольована форма. Наприклад :
+  const saveUsername = (e) => {
+    e.preventDefault();
+    
+    console.log("submit form");
+    console.log(e.target.elements.username.value);
   };
-  return <div onClick={handleClick}>App</div>
+  
+  return (
+    <div>
+      <h1>Forms in React <FaReact /></h1>
+
+      <form onSubmit={saveUsername}>
+        <input type="text" name="username" id="" />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
 }
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './components/App/App.jsx'
+import App from './components/App/App.jsx'
 import './index.css'
 
 import 'modern-normalize'
